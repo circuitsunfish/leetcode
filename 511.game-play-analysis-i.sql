bc03b7bfd7f06c238713be0956c52c1d1131905a
circuitsunfish <802-829-1624@ashtonmackenzie.com>
2023-11-27 17:55:01 -0500

    game-play-analysis i

---
+++ 511.game-play-analysis-i.sql
@@ -0,0 +1,18 @@
+--
+-- @lc app=leetcode id=511 lang=mysql
+--
+-- [511] Game Play Analysis I
+--
+
+-- @lc code=start
+# Write your MySQL query statement below
+
+    select 
+    player_id,
+    min(event_date) as first_login
+
+from activity
+group by player_id
+
+-- @lc code=end
+
